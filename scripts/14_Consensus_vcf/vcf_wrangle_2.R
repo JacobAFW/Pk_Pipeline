4fc50cf862a1f4f60484936d25b7a2986a855fa8
JacobAFW <jacob.westaway@my.jcu.edu.au>
2021-11-25 10:55:29 +0930

    Updates

--- scripts/14_Consensus_vcf/vcf_wrangle_2.R
+++ scripts/14_Consensus_vcf/vcf_wrangle_2.R
@@ -13,9 +13,9 @@ for (i in 1:100){
   Names <- append(Names, print(paste0("Sample_", i)))
 }
 
-read_tsv("/home/jwestaway/pk_pipeline/ZB_100/outputs/vcf_consensus/GATK/GATK_query.tsv", col_names =  Names) %>% 
+read_tsv("/home/jwestaway/pk_pipeline/ZB_100/outputs/vcf_consensus/GATK_query.tsv", col_names =  Names) %>% 
   select(X1) %>% 
-  inner_join(read_tsv("/home/jwestaway/pk_pipeline/ZB_100/outputs/vcf_consensus/samtools/samtools_query.tsv", col_names =  Names) %>% 
+  inner_join(read_tsv("/home/jwestaway/pk_pipeline/ZB_100/outputs/vcf_consensus/samtools_query.tsv", col_names =  Names) %>% 
                select(X1)) %>% 
   separate(X1, sep =" ", c("Contig", "Base", "ID", "Ref", "Alt")) %>% 
-  write_tsv("/home/jwestaway/pk_pipeline/ZB_100/outputs/vcf_consensus/variants_only_test/vcf_variant_names2.tsv")
+  write_tsv("/home/jwestaway/pk_pipeline/ZB_100/outputs/vcf_consensus/vcf_variant_names.tsv")
